81ba0d0756dd6ab254436768a28d114a07300856
Clayten Hamacher <clayten.hamacher@gmail.com>
2022-07-25 16:57:48 -0700

    misc

--- stockpile/thing/misc.rb
+++ stockpile/thing/misc.rb
@@ -62,9 +62,12 @@ module DFStock
     def link_index ; self.class.index_translation[index] end
   end
 
-  class Refuse < Thing
-    # 81 items, plus two flags which sit at [1] and [2] in the list.
+  class Refuse < Thing # 81 items, plus two flags - list is not in order as displayed
     from_list(81.times.map {|i| "Thing #{i}" })
+
+    def self.index_translation ; [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 76, 77, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89] end
+
+    def link_index ; self.class.index_translation[index] end
   end
 
   class Quality < Thing
