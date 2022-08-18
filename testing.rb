0f64cc11528f54e9a5542fdd61b7d19a4c1be140
Clayten Hamacher <clayten.hamacher@gmail.com>
2022-08-18 10:38:06 -0700

    some dev tests

--- testing.rb
+++ testing.rb
@@ -10,15 +10,36 @@ module DFStock
     }
   end
 
-  def self.test
-    testable_classes.map {|x|
-      p [:Creating, x]
-      l = x.index_translation.length
-      next if l.zero?
-      x.new(l - 1)
-    }.compact.map {|x|
-      p x
+  def self.test_all_classes
+    testable_classes.map {|k| k.instances.last }.compact.each {|x| p x }
+  end
+
+  def self.test_all_categories pile
+    pile ||= pile_at_cursor
+    raise "Select a stockpile and enable all categories" unless pile
+    pile.all_items.each(&:disable)
+    pile.categories.each {|cn,s|
+      s.features.select {|t,_,_,_| t == :array }.
+      each {|_,n1, n2, o|
+        sn = n2 || n1
+        i = s.send(sn)
+        next if i.empty?
+        f, l = i.first, i.last
+        [f,l].each &:enable
+        p [cn, sn, [f,l].map(&:name)]
+      }
     }
+  end
+
+  def self.test_all_comparators
+    items = testable_classes.map(&:instances).flatten.compact
+    puts "Checking comparators against #{items.length}"
+    Comparators.instance_methods.sort.
+      select {|mn| Comparators.instance_method(mn).arity.zero? }.
+      each {|mn|
+        num_rs = items.map(&mn).select {|x| next if x.respond_to?(:empty?) && x.empty? ; x }.length
+        p [:Checking, mn, num_rs]
+      }
   end
 
 end
